0e9a46da69ed5b3509ed03ec642c573686c87920
maxime <maxime@maxime-W55xEU>
2014-07-14 21:37:26 +0200

    adding compare view enabling allowing first to see persons entities

--- src/Perso/UgliestBundle/Controller/DefaultController.php
+++ src/Perso/UgliestBundle/Controller/DefaultController.php
@@ -10,4 +10,15 @@ class DefaultController extends Controller
     {
         return $this->render('PersoUgliestBundle:Default:index.html.twig');
     }
+    
+    public function compareAction()
+    {
+        // On récupère les articleCompetence pour l'article $article
+        $list_persons = $this->getDoctrine ()->getManager ()->getRepository ( 'PersoUgliestBundle:Person' )->findAll();
+    
+        // Puis modifiez la ligne du render comme ceci, pour prendre en compte les variables :
+        return $this->render ( 'PersoUgliestBundle:Default:compare.html.twig', array (
+                    'list_persons' => $list_persons
+               ) );
+    }
 }
